d7fd0b7ecfadaf162339d76ff685e83c9c6a70d3
cenesdeveloper <cenesdurak@gmail.com>
2025-06-21 00:01:24 -0400

    Add @Valid and constraint violation exception handlers to ErrorController

--- src/main/java/com/blog/blogbackend/controllers/ErrorController.java
+++ src/main/java/com/blog/blogbackend/controllers/ErrorController.java
@@ -2,10 +2,13 @@ package com.blog.blogbackend.controllers;
 
 import com.blog.blogbackend.dtos.ApiErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,6 +61,37 @@ public class ErrorController {
                 .message(exception.getMessage())
                 .build();
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        StringBuilder errorMsg = new StringBuilder("Validation failed: ");
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errorMsg.append(error.getField()).append(" - ").append(error.getDefaultMessage()).append("; ");
+        }
+
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(errorMsg.toString())
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        String errorMsg = ex.getConstraintViolations()
+                .stream()
+                .map(v -> v.getPropertyPath() + " - " + v.getMessage())
+                .reduce("", (a, b) -> a + b + "; ");
+
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(errorMsg)
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
 }

b25c048da750e916282cf25eda42e0f754eb5f63
cenesdeveloper <cenesdurak@gmail.com>
2025-07-25 13:32:50 -0400

    Add users

--- src/main/java/com/blog/blogbackend/controllers/ErrorController.java
+++ src/main/java/com/blog/blogbackend/controllers/ErrorController.java
@@ -4,6 +4,7 @@ import com.blog.blogbackend.domain.dtos.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,5 +41,15 @@ public class ErrorController {
                 .build();
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadCredentialsException(BadCredentialsException exception) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("Incorrect username or password")
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+    }
+
 
 }
---
+++ src/main/java/com/blog/blogbackend/security/BlogUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.blog.blogbackend.security;
+
+import com.blog.blogbackend.domain.entities.User;
+import com.blog.blogbackend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with email: " + email));
+        return new BlogUserDetails(user);
+    }
+}
